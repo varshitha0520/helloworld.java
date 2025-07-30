cec78ba976ba850ac727313cb84827b7ed1ed246
varshitha0520 <varshu23271@gmail.com>
2025-07-30 13:43:39 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,6 @@
 public class helloworld {
     public static void main(String[] args) {
         System.out.println("Hello, World! am good");
+        System.out.println("Hello, good afternoon");
     }
 }
